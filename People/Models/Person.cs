706d06bbc398c4fd747afad1d552ee496a27692f
MartinBurga <mburga.xixv@gmail.com>
2025-06-26 22:32:34 -0500

    Creacion de modelo Person. Se agregaron los atributos clave con sus respectivas etiquetas para la BD

--- People/Models/Person.cs
+++ People/Models/Person.cs
@@ -1,4 +1,11 @@
-﻿namespace People.Models;
+﻿using SQLite;
+namespace People.Models;
+
+[Table("People")]
 public class Person
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+    [Unique, MaxLength(250)]
+    public string Name { get; set; }
 }
